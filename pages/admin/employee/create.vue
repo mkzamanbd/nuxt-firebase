a47ce25c506bd0d7f77a1a41d564c91471039e8e
KAMRUZZAMAN <zaman.eyz@gmail.com>
2021-01-12 11:13:15 +0600

    fixing error

--- pages/admin/employee/create.vue
+++ pages/admin/employee/create.vue
@@ -35,84 +35,84 @@
 
                             <div class="row g-3 justify-content-center">
                                 <!-- type text -->
-                                <div class="col-8">
+                                <div class="col-md-8">
                                     <div class="row mb-2">
-                                        <div class="col-3">
+                                        <div class="col-md-3">
                                             <label for="inline-text" class="form-label required mt-1">Name</label>
                                         </div>
-                                        <div class="col-9">
-                                            <input type="text" class="form-control" v-model="form.name" id="inline-text" placeholder="Name" autofocus>
+                                        <div class="col-md-9">
+                                            <input type="text" class="form-control" v-model="form.name" id="inline-text" placeholder="Name" required autofocus>
                                         </div>
                                     </div>
 
                                     <div class="row mb-2">
-                                        <div class="col-3">
+                                        <div class="col-md-3">
                                             <label for="corporate-number" class="form-label required mt-1">Corporate Number</label>
                                         </div>
-                                        <div class="col-9">
+                                        <div class="col-md-9">
                                             <input type="text" class="form-control" v-model="form.corporate_number" id="corporate-number" placeholder="01X XX XXX XXX">
                                         </div>
                                     </div>
 
                                     <div class="row mb-2">
-                                        <div class="col-3">
+                                        <div class="col-md-3">
                                             <label for="personal-number" class="form-label required mt-1">Personal Number</label>
                                         </div>
-                                        <div class="col-9">
-                                            <input type="text" class="form-control" v-model="form.personal_number" id="corporate-number" placeholder="01X XX XXX XXX">
+                                        <div class="col-md-9">
+                                            <input type="text" class="form-control" v-model="form.personal_number" id="personal-number" placeholder="01X XX XXX XXX">
                                         </div>
                                     </div>
 
                                     <div class="row mb-2">
-                                        <div class="col-3">
+                                        <div class="col-md-3">
                                             <label for="bcs-batch" class="form-label required mt-1">BCS Batch</label>
                                         </div>
-                                        <div class="col-9">
+                                        <div class="col-md-9">
                                             <input type="text" class="form-control" v-model="form.bcs_batch" id="bcs-batch" placeholder="BCS Batch">
                                         </div>
                                     </div>
 
                                     <div class="row mb-2">
-                                        <div class="col-3">
+                                        <div class="col-md-3">
                                             <label for="facebook" class="form-label required mt-1">Facebook</label>
                                         </div>
-                                        <div class="col-9">
+                                        <div class="col-md-9">
                                             <input type="text" class="form-control" v-model="form.facebook" id="facebook" placeholder="Facebook Link">
                                         </div>
                                     </div>
 
                                     <div class="row mb-2">
-                                        <div class="col-3">
+                                        <div class="col-md-3">
                                             <label for="blood-group" class="form-label required mt-1">Blood Group</label>
                                         </div>
-                                        <div class="col-9">
+                                        <div class="col-md-9">
                                             <input type="text" class="form-control" v-model="form.blood_group" id="blood-group" placeholder="Blood Group">
                                         </div>
                                     </div>
 
                                     <div class="row mb-2">
-                                        <div class="col-3">
+                                        <div class="col-md-3">
                                             <label for="official-email" class="form-label required mt-1">Official Email Address</label>
                                         </div>
-                                        <div class="col-9">
+                                        <div class="col-md-9">
                                             <input type="text" class="form-control" v-model="form.email" id="official-email" placeholder="Official Email Address">
                                         </div>
                                     </div>
 
                                     <div class="row mb-2">
-                                        <div class="col-3">
+                                        <div class="col-md-3">
                                             <label for="division" class="form-label required mt-1">Location</label>
                                         </div>
-                                        <div class="col-9">
+                                        <div class="col-md-9">
                                             <div class="row">
-                                                <div class="col-6">
+                                                <div class="col-md-6">
                                                     <select class="form-select" id="division" v-model="form.division_id" @change="getDistricts">
                                                         <option value="" selected>Choose Division</option>
                                                         <option :value="division.id" v-for="(division, index) in divisions" :key="index">{{ division.name }}</option>
                                                     </select>
                                                 </div>
 
-                                                <div class="col-6">
+                                                <div class="col-md-6">
                                                     <select class="form-select" id="district" v-model="form.district_id" @change="getUpazilas">
                                                         <option value="" selected>Choose District</option>
                                                         <option :value="district.id" v-for="(district, index) in districts" :key="index">{{ district.name }}</option>
@@ -121,40 +121,40 @@
                                             </div>
 
                                             <div class="row mt-2">
-                                                <div class="col-6">
+                                                <div class="col-md-6">
                                                     <select class="form-select" id="upazilas" v-model="form.upazila_id">
                                                         <option value="" selected>Choose Upazila</option>
                                                         <option :value="upazila.id" v-for="(upazila, index) in upazilas" :key="index">{{ upazila.name }}</option>
                                                     </select>
-                                                </div>    
+                                                </div>
 
-                                                <div class="col-6">
+                                                <div class="col-md-6">
                                                     <input type="text" class="form-control" v-model="form.address" id="address" placeholder="Address">
-                                                </div>    
-                                            </div>                                            
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
 
 
                                     <div class="row mb-2">
-                                        <div class="col-3">
+                                        <div class="col-md-3">
                                             <label for="image" class="form-label required mt-1">Photo</label>
                                         </div>
-                                        <div class="col-9">
+                                        <div class="col-md-9">
                                             <input type="file" class="form-control" id="image" @change="userImage">
-                                            <img :src="form.image" alt="image" width="100px" v-if="form.image" class="img-thumbnail">
+                                            <img :src="form.image" alt="image" width="100px" v-if="form.image" class="img-thumbnail mt-2">
                                         </div>
                                     </div>
 
                                     <div class="row">
-                                        <div class="col-9 offset-3">
+                                        <div class="col-md-9 offset-3">
                                             <div class="mb-2" v-if="progressBar > 0">
                                                 <b-progress :value="progressBar" max="100" show-progress animated></b-progress>
                                             </div>
 
                                             <button type="submit" class="btn custom-btn btn-success me-2">Submit</button>
                                             <button type="reset" class="btn custom-btn btn-secondary me-2">Reset</button>
-                                        
+
                                         </div>
                                     </div>
 
@@ -195,7 +195,7 @@ export default {
                 address: '',
                 image: ''
             },
-        
+
             progressBar: 0,
             divisions: [],
             districts: [],
@@ -262,31 +262,33 @@ export default {
             })
         },
 
-        userImage(e){
-            let file = e.target.files[0];
+        userImage(event){
+            let file = event.target.files[0];
 
             if(file){
 
                 //upload image
                 let storageRef = this.$fire.storage.ref('employees/'+ Math.random() + '_'  + file.name);
-            
+
                 let uploadTask  = storageRef.put(file);
-            
+
                 uploadTask.on('state_changed', (snapshot) => {
                     // Observe state change events such as progress, pause, and resume
                     // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-                    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
                     console.log('Upload is ' + progress + '% done');
                     this.progressBar = progress
-                
+
                 },(error) => {
                     // Handle unsuccessful uploads
-                },(response) => {
+                    console.log(error)
+                },() => {
                     // Handle successful uploads on complete
-                    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+
                     uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
                         this.form.image = downloadURL;
-                        console.log("image Uri: " + this.form.image)
+                        this.progressBar = 0;
+                        console.log("File available at: " + this.form.image)
                     });
                 });
             }
