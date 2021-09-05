e0df5f7569b448e83eff721ed60b6b956a02eee8
kamruzzaman <zaman7u@gmail.com>
2021-09-05 13:57:10 +0600

    Add firebase logo

---
+++ components/AsideComponent.vue
@@ -0,0 +1,50 @@
+<template>
+    <aside class="page-aside">
+    <!-- accordion menu -->
+
+        <!-- aside brand -->
+        <div class="aside-brand">
+            <nuxt-link to="/">
+                <img src="https://www.gstatic.com/devrel-devsite/prod/v33e7b2dc41690dd155c39da725d6a2a9b37af85c625b82149b8b1a06c071a1a2/firebase/images/lockup.png" alt="">
+            </nuxt-link>
+        </div>
+        <!-- End aside-brand -->
+
+        <ul class="accordion" id="asideAccordion">
+            <h6 class="ps-3 py-1">Basic</h6>
+            <li class="accordion-item">
+                <nuxt-link to="/" class="single-nav-link">
+                    <i class="bi bi-house"></i>
+                    <span>Dashboard</span>
+                </nuxt-link>
+            </li>
+
+            <li class="accordion-item">
+                <nuxt-link to="/employee" class="single-nav-link">
+                    <i class="bi bi-people"></i>
+                    <span>Employee</span>
+                </nuxt-link>
+            </li>
+
+        </ul>
+    </aside>
+</template>
+
+<script>
+    export default {
+        name:'AsideComponent',
+        data(){
+            return {
+                isActive: ''
+            }
+        },
+
+        mounted() {
+            
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/PageAsideLayer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="page-aside-layer" @click="PageAsideLayer"></div>
+</template>
+
+<script>
+    export default {
+        name:'PageAsideLayer',
+        methods:{
+            PageAsideLayer() {
+                document.querySelector(".page-aside-layer").classList.remove("show")
+                document.querySelector(".page-aside").classList.remove("hide")
+                document.querySelector(".mainbar").classList.remove("expand")
+            }
+        }
+    }
+</script>
