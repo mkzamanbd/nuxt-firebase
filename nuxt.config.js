8a5431fe82119e00e73e406db00d5253d1dfae6c
kamruzzaman <zaman7u@gmail.com>
2021-12-14 11:00:01 +0600

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
     // Global CSS (https://go.nuxtjs.dev/config-css)
     css: [
         // master scss
-        '@/assets/template/master.scss'
+        '~/assets/template/master.scss'
     ],
 
     // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
