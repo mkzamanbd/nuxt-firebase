aed3e138d27c4d812d45d0ee18191faf05835744
Atik Faysal <zamandotbd@gmail.com>
2022-06-20 20:42:28 +0600

    added notification icon

--- pages/notification.vue
+++ pages/notification.vue
@@ -22,7 +22,7 @@
                             <th class="px-4 py-3">Title(android.title)</th>
                             <th class="px-4 py-3">Text(android.text)</th>
                             <th class="px-4 py-3">Created At</th>
-                            <th class="px-4 py-3">Action</th>
+                            <th class="px-4 py-3"></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -37,8 +37,8 @@
 
                             <td class="px-4 py-3">
                                 <div class="space-x-2 flex">
-                                    <button type="button" class="flex items-center justify-center h-8 w-8 rounded leading-5 ripple bg-red-500 text-white" @click="deleteNotification(notification)">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                                    <button type="button" class="flex items-center justify-center" @click="deleteNotification(notification)">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill fill-red-500" viewBox="0 0 16 16">
                                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
                                         </svg>
                                     </button>
@@ -148,6 +148,6 @@
 </script>
 <style scoped>
     .h-100vh{
-        height: calc(100vh - 150px);
+        height: calc(100vh - 200px);
     }
 </style>
